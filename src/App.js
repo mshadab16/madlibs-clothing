7d597e64fb59468d2c092874b3ae3043d0667f55
Mohammad Shadab <mshadab16@gmail.com>
2020-09-03 17:18:18 +0530

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,13 +33,10 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-          console.log(this.state);
         });
-
       } else {
         this.setState({ currentUser: userAuth })
       }
-
     });
   }
 
